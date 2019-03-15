133843a8d72078715789ecc42bc9d03e4774438a
FireBelow <FireBelow@users.noreply.github.com>
2019-03-15 01:01:54 -0400

    New Forecast Weather script

---
+++ GetForecast.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# GetForecast.py
+
+from weightfunctions import read_scale, write_file, get_weather, IFTTTmsg, calculate, check_web_response, weather_date_only, get_rain_json, get_open_forecast
+
+get_open_forecast()
